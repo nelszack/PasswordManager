df60cde4c63a42303639c6078aa1dfb50e042f0b
nelszack <nel20012@byui.edu>
2026-01-22 14:33:37 -0700

    feat(config): add basic config

---
+++ src/config.rs
@@ -0,0 +1,68 @@
+use crate::ConfigArgs;
+use serde::{Deserialize, Serialize};
+use std::{fs, path::Path};
+const CONFIG_PATH:&str="config.toml";
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub genpass: Genpassconf,
+    pub clpboard: Clpbconf,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Genpassconf {
+    pub length: u8,
+    pub no_stats: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Clpbconf {
+    pub timeout: u8,
+}
+
+fn default_config(write_to_file: bool) -> Config {
+    let config = Config {
+        genpass: Genpassconf {
+            length: 12,
+            no_stats: false,
+        },
+        clpboard: Clpbconf { timeout: 10 },
+    };
+    if write_to_file {
+        let toml_string = toml::to_string(&config).unwrap();
+        fs::write(CONFIG_PATH, &toml_string).unwrap();
+    }
+    return config;
+}
+fn is_config() -> bool {
+    if Path::new(CONFIG_PATH).exists() {
+        return true;
+    }
+    return false;
+}
+pub fn read_config() -> Config {
+    if !is_config() {
+        return default_config(true);
+    }
+    let text = std::fs::read_to_string(CONFIG_PATH).unwrap();
+    let config: Config = toml::from_str(&text).unwrap();
+    return config;
+}
+
+pub fn update(modify: ConfigArgs) {
+    let mut config: Config = read_config();
+    if modify.defalt {
+        config = default_config(false);
+    }
+    if let Some(i) = modify.genpass_length {
+        config.genpass.length = i;
+    }
+    if let Some(i) = modify.genpass_no_stats {
+        config.genpass.no_stats = i
+    }
+    if let Some(i) = modify.clpb_timeout {
+        config.clpboard.timeout = i
+    }
+    let toml_string = toml::to_string_pretty(&config).unwrap();
+    fs::write(CONFIG_PATH, &toml_string).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod clpboard;
 mod password;
+mod config;
 use clap::{Args, Parser, Subcommand};
 use clpboard::*;
 use password::*;
+use config::*;
+
 
 #[derive(Parser)]
 struct Cli {
@@ -14,14 +17,15 @@ enum Commands {
     Genpass(GenpassArgs),
     Passcheck(PasscheckArgs),
     Clpb(ClpbArgs),
+    Config(ConfigArgs)
 }
 
 #[derive(Args)]
 struct GenpassArgs {
-    #[arg(short, long, default_value_t = 12)]
-    length: u8,
-    #[arg(long("no-stats"), default_value_t = false)]
-    stats: bool,
+    #[arg(short, long)]
+    length: Option<u8>,
+    #[arg(long("no-stats"))]
+    no_stats: Option<bool>,
 }
 #[derive(Args)]
 struct PasscheckArgs {
@@ -29,15 +33,31 @@ struct PasscheckArgs {
 }
 #[derive(Args)]
 struct ClpbArgs {
-    #[arg(short, long, default_value_t = 10)]
-    timeout: u8,
+    #[arg(short, long)]
+    timeout: Option<u8>,
 }
+#[derive(Args)]
+struct ConfigArgs{
+    #[arg(long)]
+    defalt:bool,
+    #[arg(long("genpass-length"))]
+    genpass_length:Option<u8>,
+    #[arg(long("genpass-no-stats"))]
+    genpass_no_stats:Option<bool>,
+    #[arg(long("clpb-timeout"))]
+    clpb_timeout:Option<u8>
+
+}
+
 fn main() {
     let cli = Cli::parse();
+    let conf=read_config();
+
     match cli.command {
-        Some(Commands::Genpass(command)) => gen_pass(command.length, command.stats),
+        Some(Commands::Genpass(command)) => gen_pass(command.length.unwrap_or(conf.genpass.length), command.no_stats.unwrap_or(conf.genpass.no_stats)),
         Some(Commands::Passcheck(command)) => pass_str(&command.password),
-        Some(Commands::Clpb(command)) => cpy("testpass", command.timeout),
+        Some(Commands::Clpb(command)) => cpy("testpass", command.timeout.unwrap_or(conf.clpboard.timeout)),
+        Some(Commands::Config(command)) => update(command),
         None => println!("no args todo gui"),
     }
 }
