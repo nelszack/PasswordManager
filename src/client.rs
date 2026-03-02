04c46824e73391e2922ffc6b33f29f1ba4ad167e
nelszack <131796578+nelszack@users.noreply.github.com>
2026-03-02 14:17:20 -0700

    web extention
    * http requests work
    
    * basic web extention

--- src/client.rs
+++ src/client.rs
@@ -2,20 +2,18 @@ use crate::server::ADDR;
 use crate::types::*;
 use bincode;
 use std::{
-    io::{
-        Read,
-        Write,
-    },
+    io::{Read, Write},
     net::TcpStream,
 };
 
 pub fn manager(command: ServerCommands) {
-    send_command(command);
+    send_command(command)
 }
 fn send_command(cmd: ServerCommands) {
     let mut con = TcpStream::connect(ADDR).unwrap();
     let data = bincode::serialize(&cmd).unwrap();
     con.write_all(&(data.len() as u32).to_be_bytes()).unwrap();
+    con.flush().unwrap();
     con.write_all(&data).unwrap();
     con.flush().unwrap();
     let mut responce: String = String::new();
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,14 @@
 use crate::{
     client::manager,
     types::*,
-    vault::{Vault, VaultEnteries, VaultFns,create_vault},
+    vault::{Vault, VaultEnteries, VaultFns, create_vault},
 };
 use bincode;
+use serde::{Deserialize, Serialize};
 use std::{
     fs::File,
     io::{ErrorKind, Read, Write},
-    net::{TcpListener, TcpStream},
+    net::{Shutdown, TcpListener, TcpStream},
     process::{Command, Stdio},
     thread,
     time::Duration,
@@ -108,20 +109,27 @@ pub fn server(key: String) {
     let mut vlt: Option<Vault> = None;
     for stream in listener.incoming() {
         let mut stream1 = stream.unwrap();
-        let msg = handler(&stream1);
+        let Some((msg, http)) = handler(&stream1) else {
+            stream1.flush().unwrap();
+            stream1.shutdown(Shutdown::Both).unwrap();
+            continue;
+        };
         match msg {
             ServerCommands::Kill => {
                 if !server_info.locked {
                     lock_vlt(&mut vlt, &mut server_info);
                 }
-                stream1.write_all(b"server killed\n").unwrap();
+                
+                respond("server killed", &mut stream1, http);
+                stream1.shutdown(Shutdown::Both).unwrap();
+
                 break;
             }
             ServerCommands::Lock(send) => {
                 if !server_info.locked && vlt.is_some() {
                     if send {
                         lock_vlt(&mut vlt, &mut server_info);
-                        stream1.write_all(b"Vault locked\n").unwrap();
+                        respond("Vault locked", &mut stream1, http);
                     }
                 }
             }
@@ -133,27 +141,28 @@ pub fn server(key: String) {
                     server_info.keypass = Some(info.key);
                     vlt.unlock_vault(&mut server_info);
                     thread::spawn(move || auto_lock(info.timeout.unwrap_or(0)));
-                    stream1.write_all(b"Vault unlocked\n").unwrap();
+                    respond("Vault unlocked", &mut stream1, http);
                 } else {
-                    stream1.write_all(b"A vault is already unlocked lock it before trying to unlock another one\n").unwrap();
-
+                    respond(
+                        "A vault is already unlocked lock it before trying to unlock another one",
+                        &mut stream1,
+                        http,
+                    );
                 }
             }
             ServerCommands::Status => {
-                stream1
-                    .write_all(
-                        format!(
-                            "status {}\n",
-                            if server_info.locked {
-                                "Locked"
-                            } else {
-                                "Unlocked"
-                            }
-                        )
-                        .as_bytes(),
-                    )
-                    .unwrap();
-
+                respond(
+                    &format!(
+                        "status {}\n",
+                        if server_info.locked {
+                            "Locked"
+                        } else {
+                            "Unlocked"
+                        }
+                    ),
+                    &mut stream1,
+                    http,
+                );
             }
             ServerCommands::New { key_path } => {
                 if !server_info.locked {
@@ -161,43 +170,43 @@ pub fn server(key: String) {
                 }
                 server_info.keypass = Some(key_path);
                 create_vault(&mut vlt, &mut server_info, true);
-                stream1.write_all(b"vault created").unwrap();
+                respond("vault created", &mut stream1, http);
             }
             ServerCommands::Add(info) => {
                 if server_info.locked {
-                    stream1.write_all(b"Vault locked\n").unwrap();
+                    respond("Vault locked", &mut stream1, http);
                 } else {
                     vlt.add_entry(info);
-                    stream1.write_all(b"entry added\n").unwrap();
+                    respond("entry added", &mut stream1, http);
                 }
             }
             ServerCommands::Delete(id) => {
                 if server_info.locked {
-                    stream1.write_all(b"Vault locked\n").unwrap();
+                    respond("Vault locked", &mut stream1, http);
                 } else {
                     vlt.delete_entry(id);
-                    stream1.write_all(b"entry deleted\n").unwrap();
+                    respond("entry deleted", &mut stream1, http);
                 }
             }
             ServerCommands::View => {
                 if !server_info.locked {
-                    vlt.view_entries(&mut stream1);
+                    vlt.view_entries(&mut stream1, http);
                 } else {
-                    stream1.write_all(b"Vault locked\n").unwrap();
+                    respond("Vault locked", &mut stream1, http);
                 }
             }
             ServerCommands::Get(a) => {
                 if !server_info.locked {
-                    vlt.get_entry(a,&mut stream1);
+                    vlt.get_entry(a, &mut stream1, http);
                 } else {
-                    stream1.write_all(b"Vault locked\n").unwrap();
+                    respond("Vault locked", &mut stream1, http);
                 }
             }
             ServerCommands::Update(a) => {
                 if !server_info.locked {
                     vlt.update_entry(a);
                 } else {
-                    stream1.write_all(b"Vault locked\n").unwrap();
+                    respond("Vault locked", &mut stream1, http);
                 }
             }
             ServerCommands::Export(path) => vlt.export(path),
@@ -205,30 +214,28 @@ pub fn server(key: String) {
                 if !server_info.locked {
                     lock_vlt(&mut vlt, &mut server_info);
                 }
-                if args.new{
+                if args.new {
+                    server_info=ServerInfo { locked: true, keypass: Some(args.key_pass) };
                     create_vault(&mut vlt, &mut server_info, false);
-                    stream1.write_all(b"vault created").unwrap();
-                }
-                else if server_info.locked {
+                    // respond("Vault locked", &mut stream1, http);
+                } else if server_info.locked {
                     server_info.keypass = Some(args.key_pass);
                     unlock_vlt(&mut vlt, &mut server_info);
                 }
 
                 vlt.import(args.path);
                 lock_vlt(&mut vlt, &mut server_info);
-                stream1.write_all(b"finished import\n").unwrap();
+                respond("finished import", &mut stream1, http);
             }
         }
+        stream1.flush().unwrap();
+        stream1.shutdown(Shutdown::Both).unwrap();
     }
 }
 
-fn handler(mut message: &TcpStream) -> ServerCommands {
+fn handle_tcp(mut message: &TcpStream) -> ServerCommands {
     let mut len_buff = [0u8; 4];
-    match message.read_exact(&mut len_buff) {
-        Ok(_) => {}
-        Err(e) if e.kind() == ErrorKind::UnexpectedEof => {}
-        Err(e) => panic!("error {}", e),
-    };
+    message.read_exact(&mut len_buff).unwrap();
     let len = u32::from_be_bytes(len_buff) as usize;
     let mut buf = vec![0u8; len];
     match message.read_exact(&mut buf) {
@@ -237,7 +244,54 @@ fn handler(mut message: &TcpStream) -> ServerCommands {
         Err(e) => panic!("error {}", e),
     };
     let msg: ServerCommands = bincode::deserialize(&buf).unwrap();
+    message.flush().unwrap();
     msg
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct HttpInfo {
+    command: String,
+    extra_info: Vec<Option<String>>,
+}
+fn handle_http(mut message: &TcpStream) -> ServerCommands {
+    let mut buf = [0u8; 1024];
+    message.flush().unwrap();
+    let size = message.read(&mut buf).unwrap();
+    let request_str = String::from_utf8_lossy(&buf[..size]);
+    let lines = request_str.lines();
+    if let Some(h) = lines.last() {
+        let request: HttpInfo = serde_json::from_str(h.trim()).unwrap();
+        match request.command {
+            val if val == "veiw".to_string() => ServerCommands::View,
+            val if val == "lock".to_string() => {
+                let lock = match &request.extra_info[0].clone().unwrap() {
+                    val if val == &"true".to_string() => true,
+                    val if val == &"false".to_string() => false,
+                    _ => panic!(""),
+                };
+                ServerCommands::Lock(lock)
+            }
+            val if val == "status".to_string() => ServerCommands::Status,
+            val if val == "get".to_string() => {
+                let url=request.extra_info[0].clone().unwrap();
+                ServerCommands::Get(DeleteType::Url(url))
+            }
+            val if val == "kill".to_string() => ServerCommands::Kill,
+            _ => panic!("not supported yet"),
+        }
+    } else {
+        panic!("")
+    }
+}
+
+fn handler(message: &TcpStream) -> Option<(ServerCommands, bool)> {
+    let mut buff = [0u8; 1024];
+    let n = message.peek(&mut buff).unwrap();
+    if n > 400 {
+        return Some((handle_http(message), true));
+    } else if n > 0 {
+        return Some((handle_tcp(message), false));
+    }
+    None
 }
 
 fn lock_vlt(vlt: &mut Option<Vault>, mut server_info: &mut ServerInfo) {
@@ -248,4 +302,21 @@ fn lock_vlt(vlt: &mut Option<Vault>, mut server_info: &mut ServerInfo) {
 
 fn unlock_vlt(vlt: &mut Option<Vault>, server_info: &mut ServerInfo) {
     vlt.unlock_vault(server_info);
+}
+pub fn respond(message: &str, stream: &mut TcpStream, http: bool) {
+    if http {
+        stream
+            .write_all(
+                format!(
+                    "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: {}\r\n\r\n{}",
+                    message.len(),
+                    message
+                )
+                .as_bytes(),
+            )
+            .unwrap()
+    } else {
+        stream.write_all(message.as_bytes()).unwrap()
+    }
+    stream.flush().unwrap();
 }
